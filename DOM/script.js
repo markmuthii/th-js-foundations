7ebe01b3129796ce380ea30d7c80e3693b07d2bd
Mark Muthii <mark.muthii@strathmore.edu>
2025-05-23 14:04:36 +0300

    DOM logic

--- DOM/script.js
+++ DOM/script.js
@@ -15,7 +15,28 @@ function checkAccess(birthYear) {
 }
 
 // Check when the form is submitted / when the button is clicked
-// Get the input from the form
-// Convert the input into a number
-// Call the checkAccess function and pass the input to it
-// Show the output on the page
+
+// Get the form
+const form = document.getElementById("form");
+// console.log({ form });
+
+// Two ways to add an event:
+// 1. Using the addEventListener method
+
+const formSubmissionHandler = (event) => {
+  event.preventDefault();
+  // Get the input from the form
+  let year = document.getElementById("year").value;
+
+  // Convert the input into a number
+  year = parseInt(year);
+
+  // Call the checkAccess function and pass the input to it
+  const access = checkAccess(year);
+
+  // Show the output on the page
+};
+
+form.addEventListener("submit", formSubmissionHandler);
+
+// 2. Using the onsubmit property
